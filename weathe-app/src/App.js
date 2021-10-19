5fd0152208566f2e8aa5b9e855aab2580a28832a
mustafa <mustafamjaber@yahoo.com>
2021-10-19 18:49:26 +0200

    added a new background photo

--- weathe-app/src/App.js
+++ weathe-app/src/App.js
@@ -1,50 +1,100 @@
-import {useState , useEffect} from 'react';
+import {useState , useEffect,useRef} from 'react';
+import { gsap,Power2,Timeline } from "gsap";
 import './index.css';
 
+//new_key = 8550103399f6481388a4ebb996bc57b3
+
+//https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=8550103399f6481388a4ebb996bc57b3
+//new_api = `https://api.weatherbit.io/v2.0/forecast/daily?city=benghazi&key=${new_key}`
+
+
+
 const App = ()=>{
-  let city_name ='tripoli'
+  const [contry,setContry]=useState('')
+  let city_name = 'tripoli'
   const API_key ='69537ab0815d8306bb4a008542f90f01'
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`; 
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`; 
+  
   const [weather,setWeather]=useState([])
   const [temperature,setTemperature]=useState(null)
-  const [contry,setContry]=useState('')
-  /* if(contry === ''){
-    city_name = 'tripoli'
-    setContry('')
+  const [loading,setLoading]=useState(true)
+let tee = useRef(null)
+  
+  
     
-  } */
+    const fetchApi = async ()=>{
+      setLoading(true)
+      try {
+      const response = await fetch (url)
+      const data = await response.json();
+      console.log(data);
+      setTemperature(Math.floor((data.main.temp - 273.15)))
+     // console.log(data);
+        setWeather(data);
+        setLoading(false)
+        city_name = contry
+      } catch (error) {
+      console.log(error);
+      }
 
+  }
+  useEffect(()=>{
+      fetchApi ()
+      Timeline.fromTo(tee, 0, {height:"0%"},{height:"100%"}); 
+      
+},[])
+  /* let tl = new TimelineLite()
+  tl.to(tee, 1, {height:"0%"},{height:"100%"}); */
+
+const search = (e)=>{
+  e.preventDefault();
   const fetchApi = async ()=>{
+    setLoading(true)
     try {
-    const response = await fetch (url)
+      //`http://api.openweathermap.org/data/2.5/weather?q=${contry}&appid=${API_key}`
+      //`pro.openweathermap.org/data/2.5/forecast/hourly?q=${contry}&appid=${API_key}`
+    const response = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${contry}&appid=${API_key}`)
     const data = await response.json();
+    
     setTemperature(Math.floor((data.main.temp - 273.15)))
-    console.log(data);
+   // console.log(data);
       setWeather(data);
+      setLoading(false)
+      city_name = contry
+      setContry('')
     } catch (error) {
     console.log(error);
     }
+
 }
-useEffect(() =>{
-  fetchApi()
+fetchApi()
+}
+
+if(loading){
+  return(
+    <div className="loading">
+       <h1 >loading.....</h1>
+    </div>
+  )
+}
+
+
   
-},[]) 
-/* if(contry == ''){
-  city_name = 'tripoli'
-  setContry('')
-  
-}  */
-//(fTemp - 32) * 5 / 9
-//https://openweathermap.org/img/w/${icon_id}.png
-console.log(temperature);
 
   return(
     <main className="App">
-      <form action="" className='form' onChange={(e)=>setContry(e.target.value)}>
-        <input type="text" placeholder="Search...." className='input' value={contry} />
+      <form action="" className='form' onSubmit={search}>
+        <input 
+        type="text"
+        placeholder="Search...." 
+        className='input' 
+        value={contry} 
+        onChange={(e)=>setContry(e.target.value)}
+        
+      />
       </form>
-      <section className="mai">
-       <h4>{weather.name} /  <span>{weather.sys.country}</span></h4>
+      <section className="mai" ref={el=>tee = el}>
+        <h4>{weather.name} /  <span>{weather.sys.country}</span></h4>
         <h2>{temperature}°C</h2>
         <h4 className='min-max'>min:<span>{Math.floor(weather.main.temp_min - 273.15)}°c</span>
                                 max:<span>{Math.floor(weather.main.temp_max - 273.15)}°c</span>
